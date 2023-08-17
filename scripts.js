83e5ee5428291be6ae1748117da1e67d072e12df
drboyle <dr.boyle@pm.me>
2023-08-16 22:25:50 -0700

    init

--- scripts.js
+++ scripts.js
@@ -1,37 +1,53 @@
 const m = 100 / 37;
-const c = 0;
 
 function convertToBoyle() {
     let celsiusValue = parseFloat(document.getElementById("celsius").value);
 
     if (isNaN(celsiusValue)) {
-        document.getElementById("result").innerText = "Please enter a valid Celsius value.";
+        document.getElementById("boyle").value = "Invalid Input";
         return;
     }
 
-    let boyleValue = (m * celsiusValue + c).toFixed(2);
-    document.getElementById("result").innerText = `Result: ${boyleValue} Boyle`;
+    let boyleValue = (m * celsiusValue).toFixed(2);
+    document.getElementById("boyle").value = boyleValue;
 }
 
 function convertToCelsius() {
     let boyleValue = parseFloat(document.getElementById("boyle").value);
 
     if (isNaN(boyleValue)) {
-        document.getElementById("result").innerText = "Please enter a valid Boyle value.";
+        document.getElementById("celsius").value = "Invalid Input";
         return;
     }
 
-    let celsiusValue = ((boyleValue - c) / m).toFixed(2);
-    document.getElementById("result").innerText = `Result: ${celsiusValue} °C`;
+    let celsiusValue = (boyleValue / m).toFixed(2);
+    document.getElementById("celsius").value = celsiusValue;
 }
 
 function convertToFahrenheit() {
     let celsiusValue = parseFloat(document.getElementById("celsius").value);
 
     if (isNaN(celsiusValue)) {
-        document.getElementById("result").innerText = "Please enter a valid Celsius value.";
+        document.getElementById("fahrenheit").value = "Invalid Input";
         return;
     }
+
+    let fahrenheitValue = (celsiusValue * 1.8 + 32).toFixed(2);
+    document.getElementById("fahrenheit").value = fahrenheitValue;
+}
+
+function convertFromFahrenheit() {
+    let fahrenheitValue = parseFloat(document.getElementById("fahrenheit").value);
+
+    if (isNaN(fahrenheitValue)) {
+        document.getElementById("celsius").value = "Invalid Input";
+        return;
+    }
+
+    let celsiusValue = ((fahrenheitValue - 32) / 1.8).toFixed(2);
+    document.getElementById("celsius").value = celsiusValue;
+}
+
 
     let fahrenheitValue = (celsiusValue * 1.8 + 32).toFixed(2);
     document.getElementById("result").innerText = `Result: ${fahrenheitValue} °F`;
