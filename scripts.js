c1192a09858ef0f0ffd73eac409f2c87e4366604
drboyle <dr.boyle@pm.me>
2023-08-16 22:57:03 -0700

    init

--- scripts.js
+++ scripts.js
@@ -12,30 +12,6 @@ function convertToBoyle() {
     document.getElementById("boyle").value = boyleValue;
 }
 
-function convertToCelsius() {
-    let boyleValue = parseFloat(document.getElementById("boyle").value);
-
-    if (isNaN(boyleValue)) {
-        document.getElementById("celsius").value = "Invalid Input";
-        return;
-    }
-
-    let celsiusValue = (boyleValue / m).toFixed(2);
-    document.getElementById("celsius").value = celsiusValue;
-}
-
-function convertToFahrenheit() {
-    let celsiusValue = parseFloat(document.getElementById("celsius").value);
-
-    if (isNaN(celsiusValue)) {
-        document.getElementById("fahrenheit").value = "Invalid Input";
-        return;
-    }
-
-    let fahrenheitValue = (celsiusValue * 1.8 + 32).toFixed(2);
-    document.getElementById("fahrenheit").value = fahrenheitValue;
-}
-
 function convertFromFahrenheit() {
     let fahrenheitValue = parseFloat(document.getElementById("fahrenheit").value);
 
@@ -47,21 +23,3 @@ function convertFromFahrenheit() {
     let celsiusValue = ((fahrenheitValue - 32) / 1.8).toFixed(2);
     document.getElementById("celsius").value = celsiusValue;
 }
-
-
-    let fahrenheitValue = (celsiusValue * 1.8 + 32).toFixed(2);
-    document.getElementById("result").innerText = `Result: ${fahrenheitValue} °F`;
-}
-
-function convertFromFahrenheit() {
-    let fahrenheitValue = parseFloat(document.getElementById("fahrenheit").value);
-
-    if (isNaN(fahrenheitValue)) {
-        document.getElementById("result").innerText = "Please enter a valid Fahrenheit value.";
-        return;
-    }
-
-    let celsiusValue = ((fahrenheitValue - 32) / 1.8).toFixed(2);
-    document.getElementById("result").innerText = `Result: ${celsiusValue} °C`;
-}
-
